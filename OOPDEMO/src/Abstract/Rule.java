07d2d9d446870cf452eb455df19d8f10ce6bd4db
kebohan ke <kebohan1@gmail.com>
2019-01-06 00:25:52 +0800

    改改改改改改

--- OOPDEMO/src/Abstract/Rule.java
+++ OOPDEMO/src/Abstract/Rule.java
@@ -3,7 +3,7 @@ package Abstract;
 import java.util.ArrayList;
 
 public interface Rule {
-	public void overRule(ArrayList<AbstractPawn> pawns);  //�٤��T�w�^�ǩM�ǤJ
+	public boolean overRule(ArrayList<AbstractPawn> pawns);  //�٤��T�w�^�ǩM�ǤJ
 	public ArrayList<AbstractPawn> moveRule(ArrayList<AbstractPawn> pawns,AbstractPawn pawn, int x, int y);
-	public ArrayList<AbstractPawn> eatRule(AbstractPawn Hunter,AbstractPawn prey);
+
 }
