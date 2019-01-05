d428392b7a08521a2221b3cc8492932e273ff952
LCRH <d0542218@mail.fcu.edu.tw>
2019-01-05 17:01:34 +0800

    改改改

--- OOPDEMO/src/Abstract/AbstractChess.java
+++ OOPDEMO/src/Abstract/AbstractChess.java
@@ -3,7 +3,7 @@ package Abstract;
 import java.util.ArrayList;
 
 public abstract class AbstractChess {
-	abstract public AbstractBoard createChessBoard(ArrayList<AbstractPawn> pawns);
+	abstract public AbstractBoard createChessBoard(ArrayList<AbstractPawn> pawns,Rule rule);
 	abstract public ArrayList<AbstractPawn> createPawn();
 	abstract public Rule createRule();
 }
