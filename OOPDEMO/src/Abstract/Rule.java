46ff4958d7ad8bb12d651abaee1c9cc02a104305
kebohan ke <kebohan1@gmail.com>
2019-01-05 17:22:18 +0800

    拼了命的改

--- OOPDEMO/src/Abstract/Rule.java
+++ OOPDEMO/src/Abstract/Rule.java
@@ -4,6 +4,6 @@ import java.util.ArrayList;
 
 public interface Rule {
 	public void overRule(ArrayList<AbstractPawn> pawns);  //�٤��T�w�^�ǩM�ǤJ
-	public void moveRule(ArrayList<AbstractPawn> pawns,AbstractPawn pawn, int x, int y);
-	public void eatRule(AbstractPawn Hunter,AbstractPawn prey);
+	public ArrayList<AbstractPawn> moveRule(ArrayList<AbstractPawn> pawns,AbstractPawn pawn, int x, int y);
+	public ArrayList<AbstractPawn> eatRule(AbstractPawn Hunter,AbstractPawn prey);
 }
